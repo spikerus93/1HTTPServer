e33df7c8c90918b9be9de1c307a3c05a157acb69
master <spikerus93@rambler.ru>
2023-08-02 21:07:42 +0300

    Создание первого HTTP server

---
+++ src/main/java/ru/newgeekbrains/server.java
@@ -0,0 +1,47 @@
+package ru.newgeekbrains;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class server {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8088)){
+            while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("New client connected!");
+
+
+
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream(), StandardCharsets.UTF_8));
+
+                PrintWriter output = new PrintWriter(socket.getOutputStream());
+
+                while (!input.ready()) ;
+
+                while (input.ready()) {
+                    System.out.println(input.readLine());
+                }
+
+
+                output.println("HTTP/1.1 200 OK");
+                output.println("Content-Type: text/html; charset=utf-8");
+                output.println();
+                output.println("<h1>Привет от cервера!</h1>");
+                output.flush();
+
+                input.close();
+                output.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
